aade8a92c21af6b4f596e1f0fc80325e5473f778
Nathália Cardoso <crds.nathalia@gmail.com>
2023-06-06 22:37:18 -0300

    initial commit

---
+++ Fastlane_Teste/Fastlane_Teste/Fastlane_TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fastlane_TesteApp.swift
+//  Fastlane_Teste
+//
+//  Created by Nathalia Cardoso on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Fastlane_TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fastlane_Teste/Fastlane_TesteTests/Fastlane_TesteTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fastlane_TesteTests.swift
+//  Fastlane_TesteTests
+//
+//  Created by Nathalia Cardoso on 06/06/23.
+//
+
+import XCTest
+
+final class Fastlane_TesteTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2+2, 4)
+    }
+
+}
